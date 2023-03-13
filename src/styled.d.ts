7baa8f5232ba492fc4d86408da8d2d4c7f51c608
Kyuhyun Lee <khlee1992@naver.com>
2023-03-14 07:20:09 +0900

    Lecture #6.5

---
+++ src/styled.d.ts
@@ -0,0 +1,13 @@
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+    export interface DefaultTheme {
+        textColor: string;
+        bgColor: string;
+        accentColor: string;
+        cardBgColor: string;
+        cardTextColor: string;
+        name: string;
+    }
+}
---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    textColor: "#ecf0f1",
+    bgColor: "#2c3e50",
+    accentColor: "#1abc9c",
+    cardBgColor: "#ecf0f1",
+    cardTextColor: "#2c3e50",
+    name: "dark",
+};
+
+export const lightTheme: DefaultTheme = {
+    textColor: "#2c3e50",
+    bgColor: "#ecf0f1",
+    accentColor: "#1abc9c",
+    cardBgColor: "#2c3e50",
+    cardTextColor: "#ecf0f1",
+    name: "light",
+};
