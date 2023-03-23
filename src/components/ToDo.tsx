75f92c458bfd5bb29bb768ce96c25d47943edaa5
Kyuhyun Lee <khlee1992@naver.com>
2023-03-24 08:16:02 +0900

    Until lecture #6.15

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,19 +1,46 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text, category }: IToDo) {
-    const onClick = (newCategory: IToDo["category"]) => {};
+function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+
+    const onClick = ({ text, category, id }: IToDo) => {
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+            const newToDo = { text, id, category };
+
+            return [
+                ...oldToDos.slice(0, targetIndex),
+                newToDo,
+                ...oldToDos.slice(targetIndex + 1),
+            ];
+        });
+    };
+
     return (
         <li>
             <span>{text}</span>
-            {category !== "TO_DO" && (
-                <button onClick={() => onClick("TO_DO")}>To Do</button>
-            )}
-            {category !== "DOING" && (
-                <button onClick={() => onClick("DOING")}>Doing</button>
-            )}
-            {category !== "DONE" && (
-                <button onClick={() => onClick("DONE")}>Done</button>
-            )}
+            <button
+                name="TO_DO"
+                onClick={() => onClick({ text, id, category: "TO_DO" })}
+                disabled={category === "TO_DO"}
+            >
+                To Do
+            </button>
+            <button
+                name="DOING"
+                onClick={() => onClick({ text, id, category: "DOING" })}
+                disabled={category === "DOING"}
+            >
+                Doing
+            </button>
+            <button
+                name="DONE"
+                onClick={() => onClick({ text, id, category: "DONE" })}
+                disabled={category === "DONE"}
+            >
+                Done
+            </button>
         </li>
     );
 }
